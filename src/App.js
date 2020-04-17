e9c8bfd421959611b9e388baded67c3c66d3f6e2
Sonia Sedova <sonia.sedova@gmail.com>
2020-04-17 16:37:53 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div class="table1">
+        <div class="one">One</div>
+        <div class="two">Two</div>
+        <div class="three">Three</div>
+      </div>
+      <div class="table2">
+        <div class="four">Four</div>
+        <div class="five">Five</div>
+        <div class="six">Six</div>
+      </div>
+      <div class="table3">
+        <div class="seven">Seven</div>
+        <div class="eight">Eight</div>
+        <div class="nine">Nine</div>
+      </div>
+      </React.Fragment>
+/*      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Here is some code.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+*/
+  );
+}
+
+export default App;
